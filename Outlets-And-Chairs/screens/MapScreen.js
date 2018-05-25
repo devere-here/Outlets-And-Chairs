c042e1a75d72e4b4405c886947aeac035e584102
devere-here <stevendeverehere@gmail.com>
2018-05-24 22:54:41 -0400

    feat: can display markers at nearby cafes

--- Outlets-And-Chairs/screens/MapScreen.js
+++ Outlets-And-Chairs/screens/MapScreen.js
@@ -17,6 +17,7 @@ export default class Map extends React.Component {
         latitudeDelta: 0.0922,
         longitudeDelta: 0.0421,
       },
+      cafeLocationsState: []
     };
     this.onRegionChange = this.onRegionChange.bind(this)
     this.openSearchModal = this.openSearchModal.bind(this)
@@ -35,24 +36,30 @@ export default class Map extends React.Component {
     console.log('in the componentDidMOunt')
     navigator.geolocation.getCurrentPosition((position) => {
       let latitude = position.coords.latitude,
-          longitude = position.coords.longitude
+          longitude = position.coords.longitude,
+          cafeLocations = []
       const newRegion = {
         latitude: latitude,
         longitude: longitude,
         latitudeDelta: 0.0922,
         longitudeDelta: 0.0421,
       }
-      this.setState({ region: newRegion })
+      
 
-      const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=500&type=restaurant&key=${googlePlacesKey}`
+      const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=500&type=cafe&key=${googlePlacesKey}`
 
       console.log('about to fetch')
       fetch(url)
+        .then(res => res.json())
         .then(res => {
-          return res.json()
-        })
-        .then(res => {
-          console.log('ok is it here', res)
+          res.results.forEach(ele => {
+            console.log('the cafe is this lat is', ele.geometry.location.lat, 'long is ', ele.geometry.location.lng)
+            cafeLocations.push({lat: ele.geometry.location.lat, lng: ele.geometry.location.lng})
+          })
+
+        this.setState({ region: newRegion, cafeLocationsState: cafeLocations})
+
+          
         })
         .then(err => {
           console.log('err is ', err)
@@ -79,7 +86,18 @@ export default class Map extends React.Component {
             style={{
   position: 'absolute', top: 0, left: 0, right: 0, bottom: 0
   }}
-          />
+          >
+          {
+            this.state.cafeLocationsState.length <= 0
+              ? null
+              : this.state.cafeLocationsState.map((ele) => {
+                  return (
+                    <Marker pinColor="blue" coordinate={{ latitude: ele.lat, longitude: ele.lng }} title="foo title" description="foo description" />
+                  )
+                })
+          }
+            
+          </MapView>
       </View>
     );
   }
