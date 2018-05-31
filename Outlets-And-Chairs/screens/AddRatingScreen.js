f894729271a6f080e883659aec90b8e38732a35d
devere-here <stevendeverehere@gmail.com>
2018-05-31 15:34:27 -0400

    feat: text box that appears after a review. redirects to mapScreen after user presses ok

--- Outlets-And-Chairs/screens/AddRatingScreen.js
+++ Outlets-And-Chairs/screens/AddRatingScreen.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, TextInput } from 'react-native'
+import { StyleSheet, TextInput, Alert } from 'react-native'
 import { H2, Container, Content } from 'native-base'
 import { FormLabel, Slider, Button } from 'react-native-elements'
 import { db } from '../config/firebase'
@@ -60,6 +60,7 @@ export default class AddRating extends React.Component {
   }
 
   render() {
+    const { navigate } = this.props.navigation
     return (
       <Container>
         <Content padder>
@@ -104,28 +105,38 @@ export default class AddRating extends React.Component {
             onChangeText = {(userReview) => this.setState({userReview})}
             style={styles.formItems}
           />
-          <Button title="Add Rating" onPress={() => {
-            let newAverageRatings = calculateNewAverageRatings(this.state)
-            db.collection('ratings').doc(this.props.navigation.state.params.id)
-            .set({
-              averageOverallRating: newAverageRatings.averageOverallRating.toFixed(1),
-              averageSeatingRating: newAverageRatings.averageSeatingRating.toFixed(1),
-              averageOutletRating: newAverageRatings.averageOutletRating.toFixed(1),
-              averageRestroomRating: newAverageRatings.averageRestroomRating.toFixed(1),
-              numberOfRatings: newAverageRatings.numberOfRatings,
-              latitude: this.props.navigation.state.params.latitude,
-              longitude: this.props.navigation.state.params.longitude,
-              id: this.props.navigation.state.params.id
-            })
+          <Button
+            title="Add Rating" onPress={() => {
+              let newAverageRatings = calculateNewAverageRatings(this.state)
+              db.collection('ratings').doc(this.props.navigation.state.params.id)
+              .set({
+                averageOverallRating: newAverageRatings.averageOverallRating.toFixed(1),
+                averageSeatingRating: newAverageRatings.averageSeatingRating.toFixed(1),
+                averageOutletRating: newAverageRatings.averageOutletRating.toFixed(1),
+                averageRestroomRating: newAverageRatings.averageRestroomRating.toFixed(1),
+                numberOfRatings: newAverageRatings.numberOfRatings,
+                latitude: this.props.navigation.state.params.latitude,
+                longitude: this.props.navigation.state.params.longitude,
+                id: this.props.navigation.state.params.id
+              })
 
-            if (this.state.userReview) {
-              let date = new Date()
-              console.log('this.state.userReview', this.state.userReview, 'date', `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`)
-              db.collection('reviews').doc(this.props.navigation.state.params.id).collection('reviews')
-              .doc()
-              .set({userReview: this.state.userReview,
-                    date: `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`})
-            }
+              if (this.state.userReview) {
+                let date = new Date()
+                console.log('this.state.userReview', this.state.userReview, 'date', `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`)
+                db.collection('reviews').doc(this.props.navigation.state.params.id).collection('reviews')
+                .doc()
+                .set({userReview: this.state.userReview,
+                      date: `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`})
+              }
+
+              Alert.alert(
+                'Thank You',
+                'We appreciate that you were able to submit a review',
+                [
+                  {text: 'Ok', onPress: () => navigate('Map')}
+                ],
+                { cancelable: false }
+              )
 
           }} />
         </Content>
