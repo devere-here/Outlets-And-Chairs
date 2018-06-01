9a031e10faedecda45546d128fc97798475bf7bc
devere-here <stevendeverehere@gmail.com>
2018-06-01 16:04:08 -0400

    feat: rating correctly show rating with star images if exists

--- Outlets-And-Chairs/screens/CafeRatingScreen.js
+++ Outlets-And-Chairs/screens/CafeRatingScreen.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { StyleSheet, View } from 'react-native'
+import { StyleSheet, View, Text } from 'react-native'
+import { Rating } from 'react-native-elements'
 import { H1, H2} from 'native-base'
 import { db } from '../firebase'
 
@@ -7,10 +8,10 @@ export default class CafeRating extends React.Component {
     constructor(props){
       super(props)
       this.state = {
-        overallRating: 'N/A',
-        seatingRating: 'N/A',
-        outletRating: 'N/A',
-        restroomRating: 'N/A',
+        overallRating: '',
+        seatingRating: '',
+        outletRating: '',
+        restroomRating: '',
       }
     }
 
@@ -29,10 +30,62 @@ export default class CafeRating extends React.Component {
         return (
             <View>
                 <H1>{this.props.navigation.state.params.name}</H1>
-                <H2>Overall Rating: {this.state.overallRating}</H2>
-                <H2>Seating Rating: {this.state.seatingRating}</H2>
-                <H2>Outlet Rating: {this.state.outletRating}</H2>
-                <H2>Restroom Rating: {this.state.restroomRating}</H2>
+                {!this.state.overallRating
+                ? <Text>There are currently ratings for this cafe</Text>
+                : (
+                    <View>
+                        <H2>Overall Rating: {this.state.overallRating}</H2>
+                        <Rating
+                            showRating
+                            type="star"
+                            fractions={1}
+                            startingValue={+this.state.overallRating}
+                            readonly={true}
+                            imageSize={40}
+                            ratingBackgroundColor="grey"
+                            showReadOnlyText={false}
+                            style={{ paddingVertical: 10 }}
+                        />
+                        <H2>Seating Rating: {this.state.seatingRating}</H2>
+                        <Rating
+                            showRating
+                            type="star"
+                            fractions={1}
+                            startingValue={+this.state.seatingRating}
+                            readonly={true}
+                            imageSize={40}
+                            ratingBackgroundColor="grey"
+                            showReadOnlyText={false}
+                            style={{ paddingVertical: 10 }}
+                        />
+                        <H2>Outlet Rating: {this.state.outletRating}</H2>
+                        <Rating
+                            showRating
+                            type="star"
+                            fractions={1}
+                            startingValue={+this.state.outletRating}
+                            readonly={true}
+                            imageSize={40}
+                            ratingBackgroundColor="grey"
+                            showReadOnlyText={false}
+                            style={{ paddingVertical: 10 }}
+                        />
+                        <H2>Restroom Rating: {this.state.restroomRating}</H2>
+                        <Rating
+                            showRating
+                            type="star"
+                            fractions={1}
+                            startingValue={+this.state.restroomRating}
+                            readonly={true}
+                            imageSize={40}
+                            ratingBackgroundColor="grey"
+                            showReadOnlyText={false}
+                            style={{ paddingVertical: 10 }}
+                        />
+                    </View>
+
+                )}
+                
             </View>
         )
     }
